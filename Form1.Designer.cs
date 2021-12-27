72032eb4920d1a9777f9c210446a858a63d5cb35
Berkay Karadeniz <43043692+aliberkarad@users.noreply.github.com>
2021-12-27 23:14:35 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,261 @@
+﻿
+namespace WinFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.linkLabel3 = new System.Windows.Forms.LinkLabel();
+            this.linkLabel5 = new System.Windows.Forms.LinkLabel();
+            this.label15 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(201, 178);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(95, 30);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Araştır";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 186);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(169, 22);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox1_KeyDown);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 135);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(284, 15);
+            this.progressBar1.TabIndex = 4;
+            this.progressBar1.Visible = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(67, 153);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 21);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "ÜRÜN";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(13, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 17);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Trendyol";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(12, 13);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 20);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Siteler";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label4.Location = new System.Drawing.Point(97, 13);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(115, 20);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Ürün Fiyatı(TL)";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label5.Location = new System.Drawing.Point(218, 13);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(90, 20);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Web Adresi";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.ImageAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.label6.Location = new System.Drawing.Point(150, 46);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(12, 15);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "-";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.ImageAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.label8.Location = new System.Drawing.Point(150, 102);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(12, 15);
+            this.label8.TabIndex = 12;
+            this.label8.Text = "-";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.ImageAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.label11.Location = new System.Drawing.Point(150, 74);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(12, 15);
+            this.label11.TabIndex = 15;
+            this.label11.Text = "-";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label13.Location = new System.Drawing.Point(15, 73);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(28, 17);
+            this.label13.TabIndex = 17;
+            this.label13.Text = "N11";
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(245, 46);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(16, 15);
+            this.linkLabel1.TabIndex = 18;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "...";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // linkLabel3
+            // 
+            this.linkLabel3.AutoSize = true;
+            this.linkLabel3.Location = new System.Drawing.Point(245, 73);
+            this.linkLabel3.Name = "linkLabel3";
+            this.linkLabel3.Size = new System.Drawing.Size(16, 15);
+            this.linkLabel3.TabIndex = 20;
+            this.linkLabel3.TabStop = true;
+            this.linkLabel3.Text = "...";
+            this.linkLabel3.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel3_LinkClicked);
+            // 
+            // linkLabel5
+            // 
+            this.linkLabel5.AutoSize = true;
+            this.linkLabel5.Location = new System.Drawing.Point(245, 102);
+            this.linkLabel5.Name = "linkLabel5";
+            this.linkLabel5.Size = new System.Drawing.Size(16, 15);
+            this.linkLabel5.TabIndex = 22;
+            this.linkLabel5.TabStop = true;
+            this.linkLabel5.Text = "...";
+            this.linkLabel5.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel5_LinkClicked);
+            // 
+            // label15
+            // 
+            this.label15.AutoSize = true;
+            this.label15.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label15.Location = new System.Drawing.Point(17, 102);
+            this.label15.Name = "label15";
+            this.label15.Size = new System.Drawing.Size(58, 17);
+            this.label15.TabIndex = 26;
+            this.label15.Text = "Amazon";
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.button1;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(316, 224);
+            this.Controls.Add(this.label15);
+            this.Controls.Add(this.linkLabel5);
+            this.Controls.Add(this.linkLabel3);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.LinkLabel linkLabel3;
+        private System.Windows.Forms.LinkLabel linkLabel5;
+        private System.Windows.Forms.Label label15;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Net;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using HtmlAgilityPack;
+
+namespace WinFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        string WebAdresi, WebAdresi3, WebAdresi5;
+
+        private void textBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+           
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Process.Start("C:/Program Files/BraveSoftware/Brave-Browser/Application/brave.exe", WebAdresi);
+        }
+
+        private void linkLabel3_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Process.Start("C:/Program Files/BraveSoftware/Brave-Browser/Application/brave.exe", WebAdresi3);
+        }
+
+
+        private void linkLabel5_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Process.Start("C:/Program Files/BraveSoftware/Brave-Browser/Application/brave.exe", WebAdresi5);
+        }
+
+  
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            progressBar1.Value = 0;
+            progressBar1.Visible = true;
+
+            string UrunAdi = textBox1.Text;
+            string EditedUrun = UrunAdi.ToLower().Replace(" ", "%20");
+
+            progressBar1.Value = 5;
+            /*********************** TRENDYOL ***********************************/
+            WebAdresi = "https://www.trendyol.com/sr?q=" + EditedUrun + "&qt=" + EditedUrun + "&st=" + EditedUrun + "&os=1&sst=PRICE_BY_ASC";
+            WebRequest SiteyeBaglantiTalebi0 = WebRequest.Create(WebAdresi);
+            WebResponse GelenCevap0 = SiteyeBaglantiTalebi0.GetResponse();
+            StreamReader CevapOku0 = new StreamReader(GelenCevap0.GetResponseStream());
+            string CevapText0 = CevapOku0.ReadToEnd();
+            
+            int IndexBaslangici0 = CevapText0.IndexOf("promotion");
+            IndexBaslangici0 = CevapText0.IndexOf("sllng", IndexBaslangici0);
+            IndexBaslangici0 = CevapText0.IndexOf(">", IndexBaslangici0)+1;
+            int IndexBitisi0 = CevapText0.Substring(IndexBaslangici0).IndexOf("TL");
+            label6.Text = CevapText0.Substring(IndexBaslangici0, IndexBitisi0);
+            linkLabel1.Text = "link";
+            GelenCevap0.Close();
+            CevapOku0.Close();
+            SiteyeBaglantiTalebi0.Abort();
+            /*******************************************************************/
+            progressBar1.Value = 17;
+            /*********************** HepsiBurada ***********************************
+                            HTTP REQUEST KISITLAMASI VAR(403)
+            *******************************************************************/
+            progressBar1.Value = 34;
+            /********************************N11********************************/
+            WebAdresi3 = "https://www.n11.com/arama?q=" + EditedUrun + "&srt=PRICE_LOW";
+            WebRequest SiteyeBaglantiTalebi3 = WebRequest.Create(WebAdresi3);
+            WebResponse GelenCevap3 = SiteyeBaglantiTalebi3.GetResponse();
+            StreamReader CevapOku3 = new StreamReader(GelenCevap3.GetResponseStream());
+            string CevapText3 = CevapOku3.ReadToEnd();
+
+            int IndexBaslangici3 = CevapText3.IndexOf(@"<ins>") + 5;
+            int IndexBitisi3 = CevapText3.Substring(IndexBaslangici3).IndexOf("<");
+            label11.Text = CevapText3.Substring(IndexBaslangici3, IndexBitisi3);
+            linkLabel3.Text = "link";
+            GelenCevap3.Close();
+            CevapOku3.Close();
+            SiteyeBaglantiTalebi3.Abort();
+            /*******************************************************************/
+            progressBar1.Value = 51;
+            /***********************GittiGidiyor********************************
+                           HTTP REQUEST KISITLAMASI VAR(403)
+            *******************************************************************/
+            progressBar1.Value = 62;
+            /***********************Amazon**************************************/
+            WebAdresi5 = "https://www.amazon.com.tr/s?k=" + EditedUrun + "&s=price-asc-rank";
+            WebRequest SiteyeBaglantiTalebi5 = WebRequest.Create(WebAdresi5);
+            WebResponse GelenCevap5 = SiteyeBaglantiTalebi5.GetResponse();
+            StreamReader CevapOku5 = new StreamReader(GelenCevap5.GetResponseStream());
+            string CevapText5 = CevapOku5.ReadToEnd();
+
+            int IndexBaslangici5 = CevapText5.IndexOf("a-offscreen") + 13;
+            int IndexBitisi5 = CevapText5.Substring(IndexBaslangici5).IndexOf("TL");
+            label8.Text = CevapText5.Substring(IndexBaslangici5, IndexBitisi5);
+            linkLabel5.Text = "link";
+            GelenCevap5.Close();
+            CevapOku5.Close();
+            SiteyeBaglantiTalebi5.Abort();
+            /*******************************************************************/
+            progressBar1.Value = 78;
+            /***********************CicekSepeti*********************************
+                        SİTE FİYATLARI İŞLEM İLE HESAPLIYOR
+            *******************************************************************/
+            progressBar1.Value = 100;       
+            progressBar1.Visible = false;
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+
+
+    }
+}
